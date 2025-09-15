35695e2dd00b80b59ec8b4f1680ff229896c12c5
luq16 <luqawoniyi@gmail.com>
2025-09-15 17:41:00 +0100

    delete files

--- scripts/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -c
-cd /usr/app/
-cp -r /src/* .
-cp config/biocypher_docker_config.yaml config/biocypher_config.yaml
-poetry install
-BIOCYPHER_TEST_MODE=true python3 create_biological_knowledge_graph.py
-chmod -R 777 biocypher-log
